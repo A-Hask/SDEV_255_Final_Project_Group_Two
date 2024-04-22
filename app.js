6fd6989e8c260ac53d8165d901c5a979dc155625
Arlene Haskins <ahask93@gmail.com>
2024-04-22 19:16:37 +0000

    added create page to app.js

--- app.js
+++ app.js
@@ -5,8 +5,13 @@ const PORT = process.env.port || 3030;
 
 app.set('view engine', 'ejs');
 
+// port
+app.listen(PORT, 'localhost', () => {
+  console.log('listening for requests on port 3030');
+});
+
 app.use(express.static('public'));
-app.use(express.urlencoded({extended: true}));
+// app.use(express.urlencoded({extended: true}));
 app.use(morgan('dev'));
 app.use((req, res, next) => {
   res.locals.path = req.path;
@@ -18,12 +23,12 @@ app.get('/', (req, res) => {
   res.render('index', { title: 'Home', courses });
 });
 
+app.get('/courses/create', (req, res) => {
+  res.render('create', { title: 'Create a new course' });
+})
+
 // 404 page
 app.use((req, res) => {
   res.status(404).render('404', { title: '404' });
 });
 
-
-app.listen(3000, 'localhost', () => {
-  console.log('listening for requests on port 3000');
-});
