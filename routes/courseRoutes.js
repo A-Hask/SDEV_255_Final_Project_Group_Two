15b081e24f3f522db406bb9bb3411f46ad5caa98
ahask93 <ahask93@gmail.com>
2024-04-25 17:05:06 -0400

    fixed type in footer

--- routes/courseRoutes.js
+++ routes/courseRoutes.js
@@ -4,7 +4,7 @@ const courseController = require('../controllers/courseController');
 const router = express.Router();
 
 router.get('/', courseController.course_index);
-router.get('/create', courseController.course_create_get);
+router.get('/courses/create', courseController.course_create_get);
 router.post('/', courseController.course_create_post);
 
 module.exports = router;
