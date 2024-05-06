c9ffba7cb44e3890d716267ecbfca5f65f877524
Arlene Haskins <ahask93@gmail.com>
2024-05-06 15:37:22 -0400

    Update courseRoutes.js
    Fixed 404 error in course creation page

--- routes/courseRoutes.js
+++ routes/courseRoutes.js
@@ -3,7 +3,7 @@ const courseController = require('../controllers/courseController');
 const router = express.Router();
 
 router.get('/', courseController.course_index);
-router.get('/courses/create', courseController.course_create_get);
+router.get('/create', courseController.course_create_get);
 router.post('/', courseController.course_create_post);
 router.get('/:id', courseController.course_description);
 router.delete('/:id', courseController.course_delete);
