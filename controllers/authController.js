e7a47c839c167aaaa580be721905834fc181747b
ahask93 <ahask93@gmail.com>
2024-05-06 16:31:29 -0400

    added error handling to authController

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,26 @@
+const User = require('../models/User');
+
+// handle errors
+const handleErrors = (err) => {
+    console.log(err.message, err.code);
+    let errors = { email: '', password: '' };
+  
+    // duplicate email error
+    if (err.code === 11000) {
+      errors.email = 'that email is already registered';
+      return errors;
+    }
+  
+    // validation errors
+    if (err.message.includes('user validation failed')) {
+      Object.values(err.errors).forEach(({ properties }) => {
+        errors[properties.path] = properties.message;
+      });
+    }
+  
+    return errors;
+  }
+
 // controller actions
 module.exports.signup_get = (req, res) => {
     res.render('signup');
@@ -16,7 +39,7 @@ module.exports.signup_get = (req, res) => {
     }
     catch(err) {
       console.log(err);
-      res.status(400).send('error, user not created');
+      res.status(400).json({ errors });
     }
 }  
   
