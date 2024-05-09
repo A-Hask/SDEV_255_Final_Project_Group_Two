214489d2e4b4e4ea2204a603f055cc98e0033c50
ahask93 <ahask93@gmail.com>
2024-05-09 16:42:35 -0400

    teacher status now goes to database

--- controllers/authController.js
+++ controllers/authController.js
@@ -51,10 +51,10 @@ module.exports.signup_get = (req, res) => {
   }
   
   module.exports.signup_post = async (req, res) => {
-    const { email, password } = req.body;
+    const { email, password, teacher } = req.body;
   
     try {
-      const user = await User.create({ email, password });
+      const user = await User.create({ email, password, teacher });
       const token = createToken(user._id);
       res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
       res.status(201).json({ user: user._id });
@@ -70,7 +70,7 @@ module.exports.signup_get = (req, res) => {
     const { email, password } = req.body;
   
     try {
-        const user = await User.login(email, password);
+        const user = await User.login(email, password, teacher);
         const token = createToken(user._id);
         res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
         res.status(200).json({ user: user._id });
