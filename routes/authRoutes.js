0c0d5c1ff6f30e8e7f4605a03b02e5fe3d0922ec
ahask93 <ahask93@gmail.com>
2024-05-12 10:55:49 -0400

    rolled back some change that caused the render deploy to fail

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -7,6 +7,8 @@ authRouter.get('/signup', authController.signup_get);
 authRouter.post('/signup', authController.signup_post);
 authRouter.get('/login', authController.login_get);
 authRouter.post('/login', authController.login_post);
+authRouter.get('/schedule', authController.schedule_get);
+authRouter.post('/schedule', authController.schedule_post);
 authRouter.get('/logout', authController.logout_get);
 
 module.exports = authRouter;
